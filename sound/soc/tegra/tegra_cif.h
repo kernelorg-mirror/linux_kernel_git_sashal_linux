407fcbc30a8582947494fcf35db622c78ddf3a8e
Sheetal <sheetal@nvidia.com>
2023-03-09 10:14:23 +0000

    NVIDIA: SAUCE: ASoC: tegra: CIF: Add tegra264 support
    BugLink: https://bugs.launchpad.net/bugs/2120190
    
    - There is difference on the bit number due to number of
      channels increased to 32 in tegra264.
    - To handle bit difference update the CIF API.
    - The bit data need to be provided by the drivers where
      CIF registers bit values differ due to 32 channels support.
      For tegra264 I2S, AMX, ADX and ADMAIF are the drivers which
      will provide this data.
    
    http://nvbugs/3891214
    http://nvbugs/3893344
    
    Signed-off-by: Sheetal <sheetal@nvidia.com>
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>

--- sound/soc/tegra/tegra_cif.h
+++ sound/soc/tegra/tegra_cif.h
@@ -2,7 +2,7 @@
 /*
  * tegra_cif.h - TEGRA Audio CIF Programming
  *
- * Copyright (c) 2020 NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2020-2023 NVIDIA CORPORATION.  All rights reserved.
  *
  */
 
@@ -21,6 +21,10 @@
 #define TEGRA_ACIF_CTRL_REPLICATE_SHIFT		3
 #define TEGRA_ACIF_CTRL_TRUNCATE_SHIFT		1
 #define TEGRA_ACIF_CTRL_MONO_CONV_SHIFT		0
+
+#define TEGRA_32CH_ACIF_CTRL_AUDIO_BITS_SHIFT	11
+#define TEGRA_32CH_ACIF_CTRL_CLIENT_CH_SHIFT	14
+#define TEGRA_32CH_ACIF_CTRL_AUDIO_CH_SHIFT	19
 
 /* AUDIO/CLIENT_BITS values */
 #define TEGRA_ACIF_BITS_8			1
@@ -41,17 +45,30 @@ struct tegra_cif_conf {
 	unsigned int replicate;
 	unsigned int truncate;
 	unsigned int mono_conv;
+	unsigned int has_max_32ch;
 };
 
 static inline void tegra_set_cif(struct regmap *regmap, unsigned int reg,
 				 struct tegra_cif_conf *conf)
 {
 	unsigned int value;
+	unsigned int audio_bits_shift;
+	unsigned int audio_ch_shift;
+	unsigned int client_ch_shift;
+
+	audio_bits_shift = conf->has_max_32ch ? TEGRA_32CH_ACIF_CTRL_AUDIO_BITS_SHIFT : \
+			   TEGRA_ACIF_CTRL_AUDIO_BITS_SHIFT;
+
+	audio_ch_shift = conf->has_max_32ch ? TEGRA_32CH_ACIF_CTRL_AUDIO_CH_SHIFT : \
+			   TEGRA_ACIF_CTRL_AUDIO_CH_SHIFT;
+
+	client_ch_shift = conf->has_max_32ch ? TEGRA_32CH_ACIF_CTRL_CLIENT_CH_SHIFT : \
+			   TEGRA_ACIF_CTRL_CLIENT_CH_SHIFT;
 
 	value = (conf->threshold << TEGRA_ACIF_CTRL_FIFO_TH_SHIFT) |
-		((conf->audio_ch - 1) << TEGRA_ACIF_CTRL_AUDIO_CH_SHIFT) |
-		((conf->client_ch - 1) << TEGRA_ACIF_CTRL_CLIENT_CH_SHIFT) |
-		(conf->audio_bits << TEGRA_ACIF_CTRL_AUDIO_BITS_SHIFT) |
+		((conf->audio_ch - 1) << audio_ch_shift) |
+		((conf->client_ch - 1) << client_ch_shift) |
+		(conf->audio_bits << audio_bits_shift) |
 		(conf->client_bits << TEGRA_ACIF_CTRL_CLIENT_BITS_SHIFT) |
 		(conf->expand << TEGRA_ACIF_CTRL_EXPAND_SHIFT) |
 		(conf->stereo_conv << TEGRA_ACIF_CTRL_STEREO_CONV_SHIFT) |
