9dc81c0a0fdecd8ce822c23484b62daf64282cce
Yogish Kulkarni <yogishk@nvidia.com>
2023-10-31 20:18:22 +0530

    NVIDIA: SAUCE: depmod: Create dependency list for display modules
    BugLink: https://bugs.launchpad.net/bugs/2072591
    
    Create modules.dep and modules.dep.bin for opensrc and closedsrc
    display modules.
    
    http://nvbugs/4361148
    
    Signed-off-by: Yogish Kulkarni <yogishk@nvidia.com>
    Signed-off-by: Satish Seelamsetti <sseelamsetti@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>

--- scripts/depmod.sh
+++ scripts/depmod.sh
@@ -29,4 +29,27 @@ set -- -ae -F System.map
 if test -n "$INSTALL_MOD_PATH"; then
 	set -- "$@" -b "$INSTALL_MOD_PATH"
 fi
-exec "$DEPMOD" "$@" "$KERNELRELEASE"
+depmod_executed=false
+exit_status=0
+for config_file in $INSTALL_MOD_PATH/nv-disp-module-configs/nv-depmod-*-display.conf
+do
+	if test -f $config_file; then
+		config_file_name=$(basename -- "$config_file")
+		flavor=$(echo ${config_file_name} | cut -d'-' -f3)
+
+		exec "$DEPMOD" "$@" "$KERNELRELEASE"  -C "$config_file"
+
+		exit_status=$?
+		if [ "$exit_status" -ne 0 ]; then
+			break
+		fi
+
+		cp "$INSTALL_MOD_PATH/lib/modules/$KERNELRELEASE/modules.dep" "$INSTALL_MOD_PATH/nv-disp-module-configs/$flavor-modules.dep"
+		cp "$INSTALL_MOD_PATH/lib/modules/$KERNELRELEASE/modules.dep.bin" "$INSTALL_MOD_PATH/nv-disp-module-configs/$flavor-modules.dep.bin"
+		depmod_executed=true
+	fi
+done
+
+if [ "$exit_status" -eq 0 ] && [ "$depmod_executed" = false ]; then
+	exec "$DEPMOD" "$@" "$KERNELRELEASE"
+fi
