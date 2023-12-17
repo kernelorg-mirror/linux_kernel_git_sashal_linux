3f315afb02cec34b89030803e5c517774252b8a8
Dmitry Antipov <dmantipov@yandex.ru>
2023-12-17 13:29:02 +0200

    wifi: ath11k: refactor ath11k_wmi_tlv_parse_alloc()
    [ Upstream commit f5e6c0c4b0877e0ec0221df6c0041c0f39b6ce0f ]
    
    Since 'ath11k_wmi_tlv_parse_alloc()' always operates on
    'skb->data, skb->len' tuple, it may be simplified to pass
    the only 'skb' argument instead (which also implies
    refactoring of 'ath11k_pull_bcn_tx_status_ev()' and
    'ath11k_pull_chan_info_ev()' in the same way). Compile
    tested only.
    
    Signed-off-by: Dmitry Antipov <dmantipov@yandex.ru>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://msgid.link/20231214161117.75145-1-dmantipov@yandex.ru
    Stable-dep-of: 208d7fdb8597 ("wifi: ath11k: fix overreads in ath11k_wmi_process_csa_switch_count_event()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/wireless/ath/ath11k/wmi.h
+++ drivers/net/wireless/ath/ath11k/wmi.h
@@ -6304,8 +6304,8 @@ enum wmi_wmm_params_type {
 	WMI_WMM_PARAM_TYPE_11AX_MU_EDCA = 1,
 };
 
-const void **ath11k_wmi_tlv_parse_alloc(struct ath11k_base *ab, const void *ptr,
-					size_t len, gfp_t gfp);
+const void **ath11k_wmi_tlv_parse_alloc(struct ath11k_base *ab,
+					struct sk_buff *skb, gfp_t gfp);
 int ath11k_wmi_cmd_send(struct ath11k_pdev_wmi *wmi, struct sk_buff *skb,
 			u32 cmd_id);
 struct sk_buff *ath11k_wmi_alloc_skb(struct ath11k_wmi_base *wmi_sc, u32 len);
