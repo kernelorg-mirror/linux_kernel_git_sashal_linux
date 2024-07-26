9aa441c3ae99b93df9bb039a6eb16d460e265144
NeilBrown <neilb@suse.de>
2024-07-26 12:21:31 +1000

    nfsd: Pass 'cred' instead of 'rqstp' to some functions.
    [ Upstream commit 9fd45c16f3e3b95b458e049c77f4d7dfef673a52 ]
    
    nfsd_permission(), exp_rdonly(), nfsd_setuser(), and nfsexp_flags()
    only ever need the cred out of rqstp, so pass it explicitly instead of
    the whole rqstp.
    
    This makes the interfaces cleaner.
    
    Signed-off-by: NeilBrown <neilb@suse.de>
    Reviewed-by: Jeff Layton <jlayton@kernel.org>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Stable-dep-of: b778e0e0a167 ("NFSD: check truncate permission under inode lock")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/nfsd/vfs.h
+++ fs/nfsd/vfs.h
@@ -154,8 +154,8 @@ __be32		nfsd_readdir(struct svc_rqst *, struct svc_fh *,
 __be32		nfsd_statfs(struct svc_rqst *, struct svc_fh *,
 				struct kstatfs *, int access);
 
-__be32		nfsd_permission(struct svc_rqst *, struct svc_export *,
-				struct dentry *, int);
+__be32		nfsd_permission(struct svc_cred *cred, struct svc_export *exp,
+				struct dentry *dentry, int acc);
 
 static inline int fh_want_write(struct svc_fh *fh)
 {
