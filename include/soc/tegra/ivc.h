9eea2df460407de7f82af3530ed00a0f60f4b738
Manish Bhardwaj <mbhardwaj@nvidia.com>
2024-08-21 06:52:00 +0000

    NVIDIA: SAUCE: firmware: ivc: merge ivc_ext.c with upstream ivc.c
    BugLink: https://bugs.launchpad.net/bugs/2120190
    
    merging ivc_ext.c driver with upstream ivc.c driver
    and stop building ivc_ext.ko driver in nvidia-oot driver.
    
    http://nvbugs/4551265
    
    Signed-off-by: Manish Bhardwaj <mbhardwaj@nvidia.com>
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    Reviewed-by: Jon Hunter <jonathanh@nvidia.com>
    Tested-by: Sumit Gupta <sumitg@nvidia.com>
    Signed-off-by: Vishwaroop A <va@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>

--- include/soc/tegra/ivc.h
+++ include/soc/tegra/ivc.h
@@ -1,7 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-/*
- * Copyright (c) 2016, NVIDIA CORPORATION.  All rights reserved.
- */
+/* SPDX-FileCopyrightText: Copyright (c) 2016-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved. */
 
 #ifndef __TEGRA_IVC_H
 #define __TEGRA_IVC_H
@@ -29,6 +27,95 @@ struct tegra_ivc {
 	size_t frame_size;
 };
 
+/**
+* tegra_ivc_empty - Checks whether channel is empty or not
+* @map		pointer to iosys-map buffer for the IVC channel
+*
+* Checks whether channel is empty or not to read or write
+*
+* Returns true if channel is empty to read or write.
+*/
+bool tegra_ivc_empty(struct tegra_ivc *ivc, struct iosys_map *map);
+
+/**
+ * tegra_ivc_channel_sync - Syncs the IVC channel across reboots.
+ * @ivc		pointer of the IVC channel
+ *
+ * Syncs the IVC channel across reboots.
+ *
+ * Returns 0 on success.
+ */
+int tegra_ivc_channel_sync(struct tegra_ivc *ivc);
+
+/**
+ * tegra_ivc_frames_available - Checks number of available frames.
+ * @map		pointer to iosys-map buffer for the IVC channel
+ *
+ * Checks number of available frames.
+ *
+ * Returns integer value indicating number of available frames..
+ */
+uint32_t tegra_ivc_frames_available(struct tegra_ivc *ivc, struct iosys_map *map);
+
+/**
+ * tegra_ivc_can_read - Checks whether we can read from ivc channel
+ * @ivc		pointer of the IVC channel
+ *
+ * Checks whether we can read from ivc channel or not
+ *
+ * Returns 1 for success and 0 for failure.
+ */
+int tegra_ivc_can_read(struct tegra_ivc *ivc);
+
+/**
+ * tegra_ivc_can_write - Checks whether we can write to ivc channel
+ * @ivc		pointer of the IVC channel
+ *
+ * Checks whether we can write to ivc channel or not
+ *
+ * Returns 1 for success and 0 for failure.
+ */
+int tegra_ivc_can_write(struct tegra_ivc *ivc);
+
+/**
+ * tegra_ivc_read - Reads frame form ivc channel
+ * @ivc		pointer of the IVC channel
+ * @usr_buf	user buffer to be updated with data
+ * @buf		kernel buffer to be updated with data
+ * @max_read	max data ro be read form ivc channel
+ *
+ * Reads frame form ivc channel
+ *
+ * Returns no. of bytes read from ivc channel else return error.
+ */
+int tegra_ivc_read(struct tegra_ivc *ivc, void __user *usr_buf, void *buf, size_t max_read);
+
+/**
+ * tegra_ivc_read_peek - Reads frame form ivc channel
+ * @ivc		pointer of the IVC channel
+ * @usr_buf	user buffer to be updated with data
+ * @buf		kernel buffer to be updated with data
+ * @offset	kernel buffer to be updated with data from some offset
+ * @size	max data ro be read form ivc channel
+ *
+ * Reads frame form ivc channel from some offset
+ *
+ * Returns no. of bytes read from ivc channel else return error.
+ */
+int tegra_ivc_read_peek(struct tegra_ivc *ivc, void __user *usr_buf, void *buf, size_t offset, size_t size);
+
+/**
+ * tegra_ivc_write - Writes frame to ivc channel
+ * @ivc		pointer of the IVC channel
+ * @usr_buf	user buffer to e written to ivc channel
+ * @buf		kernel buffer to be written to ivc channel
+ * @size	Data size to be written to ivc channel
+ *
+ * Writes frame to ivc channel
+ *
+ * Returns no. of bytes written to ivc channel else return error.
+ */
+int tegra_ivc_write(struct tegra_ivc *ivc, const void __user *usr_buf, const void *buf, size_t size);
 /**
  * tegra_ivc_read_get_next_frame - Peek at the next frame to receive
  * @ivc		pointer of the IVC channel
