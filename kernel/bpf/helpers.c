49189729ee082fbb1d4eac2afd137c1f44a31a52
Hou Tao <houtao1@huawei.com>
2025-01-08 09:07:23 +0800

    bpf: Remove migrate_{disable|enable} in bpf_obj_free_fields()
    [ Upstream commit 1d2dbe7120e89090ed4f6be03d6fbadfbfff59bf ]
    
    The callers of bpf_obj_free_fields() have already guaranteed that the
    migration is disabled, therefore, there is no need to invoke
    migrate_{disable,enable} pair in bpf_obj_free_fields()'s underly
    implementation.
    
    This patch removes unnecessary migrate_{disable|enable} pairs from
    bpf_obj_free_fields() and its callees: bpf_list_head_free() and
    bpf_rb_root_free().
    
    Signed-off-by: Hou Tao <houtao1@huawei.com>
    Link: https://lore.kernel.org/r/20250108010728.207536-12-houtao@huaweicloud.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Stable-dep-of: 4a7910ee060d ("bpf: Clear rb node linkage when freeing bpf_rb_root")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/bpf/helpers.c
+++ kernel/bpf/helpers.c
@@ -2001,9 +2001,7 @@ unlock:
 		/* The contained type can also have resources, including a
 		 * bpf_list_head which needs to be freed.
 		 */
-		migrate_disable();
 		__bpf_obj_drop_impl(obj, field->graph_root.value_rec, false);
-		migrate_enable();
 	}
 }
 
@@ -2040,9 +2038,7 @@ void bpf_rb_root_free(const struct btf_field *field, void *rb_root,
 		obj -= field->graph_root.node_offset;
 
 
-		migrate_disable();
 		__bpf_obj_drop_impl(obj, field->graph_root.value_rec, false);
-		migrate_enable();
 	}
 }
 
