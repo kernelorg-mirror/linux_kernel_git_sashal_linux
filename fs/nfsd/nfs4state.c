f5b4b2a0ea12107085c916f9ba3b665fc327c709
Jeff Layton <jlayton@kernel.org>
2025-02-13 09:08:29 -0500

    nfsd: allow SC_STATUS_FREEABLE when searching via nfs4_lookup_stateid()
    [ Upstream commit d1bc15b147d35b4cb7ca99a9a7d79d41ca342c13 ]
    
    The pynfs DELEG8 test fails when run against nfsd. It acquires a
    delegation and then lets the lease time out. It then tries to use the
    deleg stateid and expects to see NFS4ERR_DELEG_REVOKED, but it gets
    bad NFS4ERR_BAD_STATEID instead.
    
    When a delegation is revoked, it's initially marked with
    SC_STATUS_REVOKED, or SC_STATUS_ADMIN_REVOKED and later, it's marked
    with the SC_STATUS_FREEABLE flag, which denotes that it is waiting for
    s FREE_STATEID call.
    
    nfs4_lookup_stateid() accepts a statusmask that includes the status
    flags that a found stateid is allowed to have. Currently, that mask
    never includes SC_STATUS_FREEABLE, which means that revoked delegations
    are (almost) never found.
    
    Add SC_STATUS_FREEABLE to the always-allowed status flags, and remove it
    from nfsd4_delegreturn() since it's now always implied.
    
    Fixes: 8dd91e8d31fe ("nfsd: fix race between laundromat and free_stateid")
    Cc: stable@vger.kernel.org
    Signed-off-by: Jeff Layton <jlayton@kernel.org>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    CVE-2025-39688
    Signed-off-by: Abdur Rahman <abdur.rahman@canonical.com>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -6558,6 +6558,8 @@ nfsd4_lookup_stateid(struct nfsd4_compound_state *cstate,
 		 */
 		statusmask |= SC_STATUS_REVOKED;
 
+	statusmask |= SC_STATUS_FREEABLE;
+
 	if (ZERO_STATEID(stateid) || ONE_STATEID(stateid) ||
 		CLOSE_STATEID(stateid))
 		return nfserr_bad_stateid;
@@ -7198,9 +7200,7 @@ nfsd4_delegreturn(struct svc_rqst *rqstp, struct nfsd4_compound_state *cstate,
 	if ((status = fh_verify(rqstp, &cstate->current_fh, S_IFREG, 0)))
 		return status;
 
-	status = nfsd4_lookup_stateid(cstate, stateid, SC_TYPE_DELEG,
-				      SC_STATUS_REVOKED | SC_STATUS_FREEABLE,
-				      &s, nn);
+	status = nfsd4_lookup_stateid(cstate, stateid, SC_TYPE_DELEG, SC_STATUS_REVOKED, &s, nn);
 	if (status)
 		goto out;
 	dp = delegstateid(s);
