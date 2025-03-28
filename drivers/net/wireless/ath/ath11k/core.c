39090b534fedcb5f4ba20dcf24276db06a36fa22
Baochen Qiang <quic_bqiang@quicinc.com>
2025-03-28 13:32:26 +0800

    wifi: ath11k: refactor ath11k_core_suspend/_resume()
    [ Upstream commit 662cc5b92c327e94587a959d7ed75862eda4b059 ]
    
    Due to [1] we currently put WLAN target into WoWLAN mode regardless of
    machine models. In upcoming patches we will support another mode, and
    finally which mode is chosen depends on the exact machine model.
    
    To prepare for such change, refactor the actual WoWLAN stuff in
    ath11k_core_suspend() into a new helper ath11k_core_suspend_wow(), this
    increase the code readability when the new suspend mode is added in
    upcoming patches.
    
    Same change applies to ath11k_core_resume();
    
    [1] https://bugzilla.kernel.org/show_bug.cgi?id=219196
    
    Tested-on: WCN6855 hw2.0 PCI WLAN.HSP.1.1-03125-QCAHSPSWPL_V1_V2_SILICONZ_LITE-3.6510.30
    
    Tested-by: Muhammad Usama Anjum <usama.anjum@collabora.com>
    Tested-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Baochen Qiang <quic_bqiang@quicinc.com>
    Link: https://patch.msgid.link/20250328-ath11k-bring-hibernation-back-v3-3-23405ae23431@quicinc.com
    Signed-off-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>
    Stable-dep-of: 8c79aac429b5 ("wifi: ath11k: cancel SSR work items during PCI shutdown")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/wireless/ath/ath11k/core.c
+++ drivers/net/wireless/ath/ath11k/core.c
@@ -903,13 +903,9 @@ static int ath11k_core_continue_suspend_resume(struct ath11k_base *ab)
 	return 1;
 }
 
-int ath11k_core_suspend(struct ath11k_base *ab)
+static int ath11k_core_suspend_wow(struct ath11k_base *ab)
 {
 	int ret;
-
-	ret = ath11k_core_continue_suspend_resume(ab);
-	if (ret <= 0)
-		return ret;
 
 	ret = ath11k_dp_rx_pktlog_stop(ab, true);
 	if (ret) {
@@ -954,15 +950,22 @@ int ath11k_core_suspend(struct ath11k_base *ab)
 
 	return 0;
 }
-EXPORT_SYMBOL(ath11k_core_suspend);
 
-int ath11k_core_resume(struct ath11k_base *ab)
+int ath11k_core_suspend(struct ath11k_base *ab)
 {
 	int ret;
 
 	ret = ath11k_core_continue_suspend_resume(ab);
 	if (ret <= 0)
 		return ret;
+
+	return ath11k_core_suspend_wow(ab);
+}
+EXPORT_SYMBOL(ath11k_core_suspend);
+
+static int ath11k_core_resume_wow(struct ath11k_base *ab)
+{
+	int ret;
 
 	ret = ath11k_hif_resume(ab);
 	if (ret) {
@@ -987,6 +990,17 @@ int ath11k_core_resume(struct ath11k_base *ab)
 	}
 
 	return 0;
+}
+
+int ath11k_core_resume(struct ath11k_base *ab)
+{
+	int ret;
+
+	ret = ath11k_core_continue_suspend_resume(ab);
+	if (ret <= 0)
+		return ret;
+
+	return ath11k_core_resume_wow(ab);
 }
 EXPORT_SYMBOL(ath11k_core_resume);
 
