c92453309cf6b39d49688f2d6e20dd216e824205
Phil Sutter <phil@nwl.cc>
2025-05-21 22:44:26 +0200

    netfilter: nf_tables: Have a list of nf_hook_ops in nft_hook
    [ Upstream commit 73319a8ee18b9cf0b2dac87f8521595e0381ba0c ]
    
    Supporting a 1:n relationship between nft_hook and nf_hook_ops is
    convenient since a chain's or flowtable's nft_hooks may remain in place
    despite matching interfaces disappearing. This stabilizes ruleset dumps
    in that regard and opens the possibility to claim newly added interfaces
    which match the spec. Also it prepares for wildcard interface specs
    since these will potentially match multiple interfaces.
    
    All spots dealing with hook registration are updated to handle a list of
    multiple nf_hook_ops, but nft_netdev_hook_alloc() only adds a single
    item for now to retain the old behaviour. The only expected functional
    change here is how vanishing interfaces are handled: Instead of dropping
    the respective nft_hook, only the matching nf_hook_ops are dropped.
    
    To safely remove individual ops from the list in netdev handlers, an
    rcu_head is added to struct nf_hook_ops so kfree_rcu() may be used.
    There is at least nft_flowtable_find_dev() which may be iterating
    through the list at the same time.
    
    Signed-off-by: Phil Sutter <phil@nwl.cc>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Stable-dep-of: f3224ee463f8 ("netfilter: nf_tables: use list_del_rcu for netlink hooks")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Abdur Rahman <abdur.rahman@canonical.com>

--- include/net/netfilter/nf_tables.h
+++ include/net/netfilter/nf_tables.h
@@ -1214,8 +1214,10 @@ struct nft_stats {
 
 struct nft_hook {
 	struct list_head	list;
-	struct nf_hook_ops	ops;
+	struct list_head	ops_list;
 	struct rcu_head		rcu;
+	char			ifname[IFNAMSIZ];
+	u8			ifnamelen;
 };
 
 struct nf_hook_ops *nft_hook_find_ops(const struct nft_hook *hook,
