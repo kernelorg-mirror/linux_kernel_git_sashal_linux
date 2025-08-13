786e02fff46b7370579991a76a2448afe7aca155
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2025-08-13 15:21:19 -0400

    Bluetooth: ISO: Use sk_sndtimeo as conn_timeout
    [ Upstream commit 339a87883a14d6a818ca436fed41aa5d10e0f4bd ]
    
    This aligns the usage of socket sk_sndtimeo as conn_timeout when
    initiating a connection and then use it when scheduling the
    resulting HCI command, similar to what has been done in bf98feea5b65
    ("Bluetooth: hci_conn: Always use sk_timeo as conn_timeout").
    
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    [ dep note: this tree does not have the BIS 'sid' parameter added by
      a later upstream commit, so hci_add_bis()/hci_bind_bis()/
      hci_connect_bis() keep their existing (no-sid) parameter lists here
      and only gain the new u16 timeout parameter. Also, secs_to_jiffies()
      is not available as a generic helper in this tree's jiffies.h (it
      only exists as a file-local macro in hci_event.c), so the
      ISO_CONN_TIMEOUT/ISO_DISCONN_TIMEOUT conversion uses
      msecs_to_jiffies() instead to keep the same 20s/2s values. ]
    
    Stable-dep-of: 2f5d635ad590 ("Bluetooth: hci_sync: hold conn in hci_connect_acl/le_sync() callbacks")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/bluetooth/iso.c
+++ net/bluetooth/iso.c
@@ -97,8 +97,8 @@ static struct sock *iso_get_sock(bdaddr_t *src, bdaddr_t *dst,
 				 iso_sock_match_t match, void *data);
 
 /* ---- ISO timers ---- */
-#define ISO_CONN_TIMEOUT	(HZ * 40)
-#define ISO_DISCONN_TIMEOUT	(HZ * 2)
+#define ISO_CONN_TIMEOUT	msecs_to_jiffies(20 * 1000)
+#define ISO_DISCONN_TIMEOUT	msecs_to_jiffies(2 * 1000)
 
 static void iso_conn_free(struct kref *ref)
 {
@@ -422,7 +422,8 @@ static int iso_connect_bis(struct sock *sk)
 	if (test_bit(BT_SK_DEFER_SETUP, &bt_sk(sk)->flags)) {
 		hcon = hci_bind_bis(hdev, &iso_pi(sk)->dst,
 				    &iso_pi(sk)->qos, iso_pi(sk)->base_len,
-				    iso_pi(sk)->base);
+				    iso_pi(sk)->base,
+				    READ_ONCE(sk->sk_sndtimeo));
 		if (IS_ERR(hcon)) {
 			err = PTR_ERR(hcon);
 			goto unlock;
@@ -431,7 +432,8 @@ static int iso_connect_bis(struct sock *sk)
 		hcon = hci_connect_bis(hdev, &iso_pi(sk)->dst,
 				       le_addr_type(iso_pi(sk)->dst_type),
 				       &iso_pi(sk)->qos, iso_pi(sk)->base_len,
-				       iso_pi(sk)->base);
+				       iso_pi(sk)->base,
+				       READ_ONCE(sk->sk_sndtimeo));
 		if (IS_ERR(hcon)) {
 			err = PTR_ERR(hcon);
 			goto unlock;
@@ -512,7 +514,8 @@ static int iso_connect_cis(struct sock *sk)
 	if (test_bit(BT_SK_DEFER_SETUP, &bt_sk(sk)->flags)) {
 		hcon = hci_bind_cis(hdev, &iso_pi(sk)->dst,
 				    le_addr_type(iso_pi(sk)->dst_type),
-				    &iso_pi(sk)->qos);
+				    &iso_pi(sk)->qos,
+				    READ_ONCE(sk->sk_sndtimeo));
 		if (IS_ERR(hcon)) {
 			err = PTR_ERR(hcon);
 			goto unlock;
@@ -520,7 +523,8 @@ static int iso_connect_cis(struct sock *sk)
 	} else {
 		hcon = hci_connect_cis(hdev, &iso_pi(sk)->dst,
 				       le_addr_type(iso_pi(sk)->dst_type),
-				       &iso_pi(sk)->qos);
+				       &iso_pi(sk)->qos,
+				       READ_ONCE(sk->sk_sndtimeo));
 		if (IS_ERR(hcon)) {
 			err = PTR_ERR(hcon);
 			goto unlock;
