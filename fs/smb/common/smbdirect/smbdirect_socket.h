0548cd0d89dfbc869684c262f0c64709c55a6cbd
Stefan Metzmacher <metze@samba.org>
2025-08-14 11:41:59 +0200

    smb: smbdirect: introduce smbdirect_socket.rw_io.credits
    [ Upstream commit 90255ed46774bda93a945a6dbf9228d9ecfcd79f ]
    
    This will be used by the server to manage the state
    for RDMA read/write requests.
    
    Cc: Steve French <smfrench@gmail.com>
    Cc: Tom Talpey <tom@talpey.com>
    Cc: Long Li <longli@microsoft.com>
    Acked-by: Namjae Jeon <linkinjeon@kernel.org>
    Cc: linux-cifs@vger.kernel.org
    Cc: samba-technical@lists.samba.org
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Stable-dep-of: fbc152311816 ("smb: smbdirect: divide, not multiply, milliseconds by 1000")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Abdur Rahman <abdur.rahman@canonical.com>

--- fs/smb/common/smbdirect/smbdirect_socket.h
+++ fs/smb/common/smbdirect/smbdirect_socket.h
@@ -52,6 +52,27 @@ struct smbdirect_socket {
 			SMBDIRECT_EXPECT_DATA_TRANSFER = 3,
 		} expected;
 	} recv_io;
+
+	/*
+	 * The state for RDMA read/write requests on the server
+	 */
+	struct {
+		/*
+		 * The credit state for the send side
+		 */
+		struct {
+			/*
+			 * The maximum number of rw credits
+			 */
+			size_t max;
+			/*
+			 * The number of pages per credit
+			 */
+			size_t num_pages;
+			atomic_t count;
+			wait_queue_head_t wait_queue;
+		} credits;
+	} rw_io;
 };
 
 static __always_inline void smbdirect_socket_init(struct smbdirect_socket *sc)
@@ -70,6 +91,9 @@ static __always_inline void smbdirect_socket_init(struct smbdirect_socket *sc)
 	INIT_LIST_HEAD(&sc->recv_io.reassembly.list);
 	spin_lock_init(&sc->recv_io.reassembly.lock);
 	init_waitqueue_head(&sc->recv_io.reassembly.wait_queue);
+
+	atomic_set(&sc->rw_io.credits.count, 0);
+	init_waitqueue_head(&sc->rw_io.credits.wait_queue);
 }
 
 struct smbdirect_send_io {
