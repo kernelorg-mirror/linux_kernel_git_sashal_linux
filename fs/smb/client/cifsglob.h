c7bc56376320778063ef5a2e0199611048cdefb9
Stefan Metzmacher <metze@samba.org>
2025-08-21 23:36:33 +0200

    smb: client: make use of struct smbdirect_mr_io
    [ Upstream commit f454f36cd0b9ee3b7b6c7181238ef20f1326063a ]
    
    This will allow us to move to common functions in future too.
    
    Cc: Steve French <smfrench@gmail.com>
    Cc: Tom Talpey <tom@talpey.com>
    Cc: Long Li <longli@microsoft.com>
    Cc: linux-cifs@vger.kernel.org
    Cc: samba-technical@lists.samba.org
    Acked-by: Namjae Jeon <linkinjeon@kernel.org>
    Signed-off-by: Stefan Metzmacher <metze@samba.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Stable-dep-of: e607ef686ab9 ("smb: client: allocate enough space for MR WRs and ib_drain_qp()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Abdur Rahman <abdur.rahman@canonical.com>

--- fs/smb/client/cifsglob.h
+++ fs/smb/client/cifsglob.h
@@ -1491,7 +1491,7 @@ struct cifs_readdata {
 	struct kvec			iov[2];
 	struct TCP_Server_Info		*server;
 #ifdef CONFIG_CIFS_SMB_DIRECT
-	struct smbd_mr			*mr;
+	struct smbdirect_mr_io		*mr;
 #endif
 	struct cifs_credits		credits;
 };
