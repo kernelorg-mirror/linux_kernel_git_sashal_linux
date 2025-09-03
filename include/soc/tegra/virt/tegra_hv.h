80c18772d7cda7d8e8e32ae68a0fd438367a6f08
Manish Bhardwaj <mbhardwaj@nvidia.com>
2025-09-03 19:25:15 +0000

    NVIDIA: SAUCE: hv-ivc: add doxygen comment
    BugLink: https://bugs.launchpad.net/bugs/2127135
    
    http://nvbugs/5433066
    
    Signed-off-by: Manish Bhardwaj <mbhardwaj@nvidia.com>
    Reviewed-by: Suresh Venkatachalam <skathirampat@nvidia.com>
    Reviewed-by: Sandeep Trasi <strasi@nvidia.com>
    Signed-off-by: Prathamesh Shete <pshete@nvidia.com>
    Acked-by: Abdur Rahman <abdur.rahman@canonical.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>

--- include/soc/tegra/virt/tegra_hv.h
+++ include/soc/tegra/virt/tegra_hv.h
@@ -29,13 +29,15 @@
 /**
  * @brief          Query IVC meta data from hypervisor
  *
- * @retval         ptr to ivc_info_page structure having ivc meta data info
+ * @retval         ptr To ivc_info_page structure having ivc meta data info
  *                 shared by hypervisor
  *
- * @pre            Tegra hypervisor driver should have been initialized.
- *                 This API should be invoked on virtual/hypervisor environment only.
+ * @pre
+ *                 - Tegra hypervisor driver should have been initialized.
+ *                 - This API should be invoked on virtual/hypervisor environment only.
  *
- * @post           IVC meta data will be retuned to client
+ * @post
+ *                 - IVC meta data will be retuned to client
  *
  * @usage
  *                 - Allowed context for the API call
@@ -54,12 +56,14 @@ const struct ivc_info_page *tegra_hv_get_ivc_info(void);
 /**
  * @brief          Query vmid of the GOS VM from hypervisor
  *
- * @retval         vmid on success else -ve value
+ * @retval         vmid On success else -ve value
  *
- * @pre            Tegra hypervisor driver should have been initialized.
- *                 This API should be invoked on virtual/hypervisor environment only.
+ * @pre
+ *                 - Tegra hypervisor driver should have been initialized.
+ *                 - This API should be invoked on virtual/hypervisor environment only.
  *
- * @post           Guest VM ID will be returned.
+ * @post
+ *                 - Guest VM ID will be returned.
  *
  * @usage
  *                 - Allowed context for the API call
