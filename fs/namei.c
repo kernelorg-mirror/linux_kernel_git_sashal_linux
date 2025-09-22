a95e4828f99c8bffe65a21d90cc0511e43887c81
NeilBrown <neil@brown.name>
2025-09-22 14:29:51 +1000

    VFS/audit: introduce kern_path_parent() for audit
    [ Upstream commit 76a53de6f7ff0641570364234fb4489f4d4fc8e9 ]
    
    audit_alloc_mark() and audit_get_nd() both need to perform a path
    lookup getting the parent dentry (which must exist) and the final
    target (following a LAST_NORM name) which sometimes doesn't need to
    exist.
    
    They don't need the parent to be locked, but use kern_path_locked() or
    kern_path_locked_negative() anyway.  This is somewhat misleading to the
    casual reader.
    
    This patch introduces a more targeted function, kern_path_parent(),
    which returns not holding locks.  On success the "path" will
    be set to the parent, which must be found, and the return value is the
    dentry of the target, which might be negative.
    
    This will clear the way to rename kern_path_locked() which is
    otherwise only used to prepare for removing something.
    
    It also allows us to remove kern_path_locked_negative(), which is
    transformed into the new kern_path_parent().
    
    Signed-off-by: NeilBrown <neil@brown.name>
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    Stable-dep-of: 81905b5acbe7 ("audit: fix recursive locking deadlock in audit_dupe_exe()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Abdur Rahman <abdur.rahman@canonical.com>

--- fs/namei.c
+++ fs/namei.c
@@ -2611,6 +2611,30 @@ static struct dentry *__kern_path_locked(int dfd, struct filename *name, struct 
 	return d;
 }
 
+/**
+ * kern_path_parent: lookup path returning parent and target
+ * @name: path name
+ * @path: path to store parent in
+ *
+ * The path @name should end with a normal component, not "." or ".." or "/".
+ * A lookup is performed and if successful the parent information
+ * is store in @parent and the dentry is returned.
+ *
+ * The dentry maybe negative, the parent will be positive.
+ *
+ * Returns:  dentry or error.
+ */
+struct dentry *kern_path_parent(const char *name, struct path *path)
+{
+	struct filename *filename = getname_kernel(name);
+	struct dentry *d = __kern_path_locked(AT_FDCWD, filename, path);
+
+	putname(filename);
+	if (!IS_ERR(d))
+		inode_unlock(path->dentry->d_inode);
+	return d;
+}
+
 struct dentry *kern_path_locked(const char *name, struct path *path)
 {
 	struct filename *filename = getname_kernel(name);
