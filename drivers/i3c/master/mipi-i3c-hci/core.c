cf3407f614ff0377971a1f77443d2a5509600ab1
Adrian Hunter <adrian.hunter@intel.com>
2026-01-13 09:26:46 +0200

    i3c: mipi-i3c-hci: Switch DAT bitmap allocation to devm_bitmap_zalloc()
    [ Upstream commit f64c1a46ea7c40bbab57a0cb1665a1c1e11da6af ]
    
    The driver already uses managed resources, so convert the Device Address
    Table (DAT) bitmap allocation to use devm_bitmap_zalloc().  Remove the
    manual cleanup routine.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Link: https://patch.msgid.link/20260113072702.16268-6-adrian.hunter@intel.com
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Stable-dep-of: 650716f23eac ("i3c: mipi-i3c-hci: Fix race in i3c_hci_addr_to_dev()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Abdur Rahman <abdur.rahman@canonical.com>

--- drivers/i3c/master/mipi-i3c-hci/core.c
+++ drivers/i3c/master/mipi-i3c-hci/core.c
@@ -164,8 +164,6 @@ static void i3c_hci_bus_cleanup(struct i3c_master_controller *m)
 	reg_clear(HC_CONTROL, HC_CONTROL_BUS_ENABLE);
 	synchronize_irq(platform_get_irq(pdev, 0));
 	hci->io->cleanup(hci);
-	if (hci->cmd == &mipi_i3c_hci_cmd_v1)
-		mipi_i3c_hci_dat_v1.cleanup(hci);
 }
 
 void mipi_i3c_hci_resume(struct i3c_hci *hci)
