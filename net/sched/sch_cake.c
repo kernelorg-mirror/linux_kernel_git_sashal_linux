36c68386783c909a2c1d95a05a0dfe7bdf89ac34
Alice Mikityanska <alice@isovalent.com>
2026-02-05 15:39:14 +0200

    net/ipv6: Introduce payload_len helpers
    [ Upstream commit b2936b4fd56294e49d6c8e9152ea6c4982757c7d ]
    
    The next commits will transition away from using the hop-by-hop
    extension header to encode packet length for BIG TCP. Add wrappers
    around ip6->payload_len that return the actual value if it's non-zero,
    and calculate it from skb->len if payload_len is set to zero (and a
    symmetrical setter).
    
    The new helpers are used wherever the surrounding code supports the
    hop-by-hop jumbo header for BIG TCP IPv6, or the corresponding IPv4 code
    uses skb_ip_totlen (e.g., in include/net/netfilter/nf_tables_ipv6.h).
    
    No behavioral change in this commit.
    
    Signed-off-by: Alice Mikityanska <alice@isovalent.com>
    Acked-by: Paolo Abeni <pabeni@redhat.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Link: https://patch.msgid.link/20260205133925.526371-2-alice.kernel@fastmail.im
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: 81be30c1f5f2 ("net/ipv6: Drop HBH for BIG TCP on RX side")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/sched/sch_cake.c
+++ net/sched/sch_cake.c
@@ -1280,7 +1280,7 @@ static struct sk_buff *cake_ack_filter(struct cake_sched_data *q,
 			    ipv6_addr_cmp(&ipv6h_check->daddr, &ipv6h->daddr))
 				continue;
 
-			seglen = ntohs(ipv6h_check->payload_len);
+			seglen = ipv6_payload_len(skb, ipv6h_check);
 		} else {
 			continue;
 		}
