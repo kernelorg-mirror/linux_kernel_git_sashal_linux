bf6b8fb0f41a1d31be8c25a11354f4e1bf70da42
Benjamin Tissoires <bentiss@kernel.org>
2026-05-04 10:47:23 +0200

    HID: core: introduce hid_safe_input_report()
    [ Upstream commit 206342541fc887ae919774a43942dc883161fece ]
    
    hid_input_report() is used in too many places to have a commit that
    doesn't cross subsystem borders. Instead of changing the API, introduce
    a new one when things matters in the transport layers:
    - usbhid
    - i2chid
    
    This effectively revert to the old behavior for those two transport
    layers.
    
    Fixes: 0a3fe972a7cb ("HID: core: Mitigate potential OOB by removing bogus memset()")
    Cc: stable@vger.kernel.org
    Signed-off-by: Benjamin Tissoires <bentiss@kernel.org>
    Signed-off-by: Jiri Kosina <jkosina@suse.com>
    Stable-dep-of: 63a694c51bf1 ("HID: uhid: convert to hid_safe_input_report()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Abdur Rahman <abdur.rahman@canonical.com>

--- include/linux/hid.h
+++ include/linux/hid.h
@@ -978,6 +978,8 @@ extern void hidinput_disconnect(struct hid_device *);
 int hid_set_field(struct hid_field *, unsigned, __s32);
 int hid_input_report(struct hid_device *hid, enum hid_report_type type, u8 *data, u32 size,
 		     int interrupt);
+int hid_safe_input_report(struct hid_device *hid, enum hid_report_type type, u8 *data,
+			  size_t bufsize, u32 size, int interrupt);
 struct hid_field *hidinput_get_led_field(struct hid_device *hid);
 unsigned int hidinput_count_leds(struct hid_device *hid);
 __s32 hidinput_calc_abs_res(const struct hid_field *field, __u16 code);
