7fb3d4eaf9dbf70d1ef3d43f7ee1dc860a28acf4
Gal Pressman <gal@nvidia.com>
2026-07-17 10:23:38 +0300

    net/mlx5: Fix MCIA register buffer overflow on 32 dword reads
    [ Upstream commit 11c057d23465c7a5817a7284c896d19d54c0b616 ]
    
    The MCIA register can return up to 32 dwords (128 bytes) when the device
    advertises the mcia_32dwords capability, but struct
    mlx5_ifc_mcia_reg_bits only defines dword_0..11, leaving room for just
    12 dwords (48 bytes) of data.
    
    mlx5_query_mcia() clamps the read size to mlx5_mcia_max_bytes() and then
    memcpy()s that many bytes out of the register, potentially reading past
    the end of the 'out' buffer. On kernels built with FORTIFY_SOURCE this
    is caught as a buffer overflow while reading the module EEPROM via
    ethtool:
    
      detected buffer overflow in memcpy
      kernel BUG at lib/string_helpers.c:1048!
      RIP: 0010:fortify_panic+0x13/0x20
      Call Trace:
       mlx5_query_mcia.isra.0+0x200/0x210 [mlx5_core]
       mlx5_query_module_eeprom_by_page+0x4a/0xa0 [mlx5_core]
       mlx5e_get_module_eeprom_by_page+0xbb/0x120 [mlx5_core]
       eeprom_prepare_data+0xf3/0x170
       ethnl_default_doit+0xf1/0x3b0
    
    Extend the mcia_reg layout to 32 dwords.
    
    Fixes: 271907ee2f29 ("net/mlx5: Query the maximum MCIA register read size from firmware")
    Signed-off-by: Gal Pressman <gal@nvidia.com>
    Reviewed-by: Alex Lazar <alazar@nvidia.com>
    Signed-off-by: Tariq Toukan <tariqt@nvidia.com>
    Link: https://patch.msgid.link/20260717072338.1240582-1-tariqt@nvidia.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/mellanox/mlx5/core/port.c
+++ drivers/net/ethernet/mellanox/mlx5/core/port.c
@@ -312,7 +312,7 @@ static int mlx5_query_module_id(struct mlx5_core_dev *dev, int module_num,
 		return -EIO;
 	}
 
-	ptr = MLX5_ADDR_OF(mcia_reg, out, dword_0);
+	ptr = MLX5_ADDR_OF(mcia_reg, out, dwords);
 
 	*module_id = ptr[0];
 
@@ -397,7 +397,7 @@ static int mlx5_query_mcia(struct mlx5_core_dev *dev,
 		return -EIO;
 	}
 
-	ptr = MLX5_ADDR_OF(mcia_reg, out, dword_0);
+	ptr = MLX5_ADDR_OF(mcia_reg, out, dwords);
 	memcpy(data, ptr, size);
 
 	return size;
