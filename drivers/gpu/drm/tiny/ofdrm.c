fa52aa913075df3ca35c2f55f885d63e31ae77f8
Shixiong Ou <oushixiong@kylinos.cn>
2026-08-25 18:41:34 +0800

    drm/sysfb: ofdrm: Fix integer overflow in fb_size calculation
    [ Upstream commit c6f48e59ece0123f6a11527ad4d89b21c2d65b87 ]
    
    The framebuffer size calculation `fb_size = linebytes * height` can
    overflow when both values are large (e.g., 46341 * 46341 > INT_MAX).
    Since linebytes and height are both int types, the multiplication is
    performed as int * int, which results in undefined behavior on overflow.
    
    Use check_mul_overflow() to detect and prevent this overflow, consistent
    with the approach used in simpledrm.c and corebootdrm.c.
    
    Signed-off-by: Shixiong Ou <oushixiong@kylinos.cn>
    Reviewed-by: Thomas Zimmermann <tzimmermann@suse.de>
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Fixes: c8a17756c425 ("drm/ofdrm: Add ofdrm for Open Firmware framebuffers")
    Cc: <stable@vger.kernel.org> # v6.2+
    Link: https://patch.msgid.link/20260825104134.669676-1-oushixiong1025@163.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/tiny/ofdrm.c
+++ drivers/gpu/drm/tiny/ofdrm.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 #include <linux/of_address.h>
+#include <linux/overflow.h>
 #include <linux/pci.h>
 #include <linux/platform_device.h>
 
@@ -1182,7 +1183,10 @@ static struct ofdrm_device *ofdrm_device_create(struct drm_driver *drv,
 			return ERR_PTR(-EINVAL);
 	}
 
-	fb_size = linebytes * height;
+	if (check_mul_overflow(linebytes, height, &fb_size)) {
+		drm_err(dev, "framebuffer size exceeds maximum\n");
+		return ERR_PTR(-EINVAL);
+	}
 
 	/*
 	 * Try to figure out the address of the framebuffer. Unfortunately, Open
