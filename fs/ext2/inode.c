eab52dd6f2f8182f07ba47740f812ea91e056e38
Jan Kara <jack@suse.cz>
2026-09-13 17:57:21 -0400

    ext2: Fix lost inode updates for IS_SYNC inodes
    [ Upstream commit 356984d1a5c32e94810cbb6c8dc7d8ff2d4d919a ]
    
    ext2_setsize() and ext2_xattr_set2() had a construct like:
    
    	if (IS_SYNC(inode)) {
    		sync_inode_metadata(inode, 1);
    	} else {
    		mark_inode_dirty(inode);
    	}
    
    which leads to lost inode updates for IS_SYNC inodes because
    sync_inode_metadata() does anything only if the inode is already dirty
    and hence inode updates may be simply lost. Fix the problem by
    unconditionally marking the inode dirty and *then* call
    sync_inode_metadata().
    
    CC: stable@vger.kernel.org
    Signed-off-by: Jan Kara <jack@suse.cz>
    Link: https://patch.msgid.link/20260727104923.3828017-26-jack@suse.cz
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/ext2/inode.c
+++ fs/ext2/inode.c
@@ -1302,11 +1302,10 @@ static int ext2_setsize(struct inode *inode, loff_t newsize)
 	filemap_invalidate_unlock(inode->i_mapping);
 
 	inode_set_mtime_to_ts(inode, inode_set_ctime_current(inode));
+	mark_inode_dirty(inode);
 	if (inode_needs_sync(inode)) {
 		sync_mapping_buffers(inode->i_mapping);
 		sync_inode_metadata(inode, 1);
-	} else {
-		mark_inode_dirty(inode);
 	}
 
 	return 0;
