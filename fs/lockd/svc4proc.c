intel-lab-lkp/linux

lockd: Rename struct nlm_res to lockd_res
As part of the effort to enable lockd's server-side XDR functions to
be generated from the NLM protocol specification (using xdrgen), the
internal type names must be changed to avoid conflicts with the
machine-generated type names.

Rename struct nlm_res to struct lockd_res to avoid conflicts with
the NLMv3 XDR type definitions that will be introduced when svcproc.c
is converted to use xdrgen.

Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

fs/lockd/svc4proc.c
@@ -562,7 +562,7 @@ static const struct rpc_call_ops nlm4svc_callback_ops = {
  */
 static __be32
 nlm4svc_callback(struct svc_rqst *rqstp, struct nlm_host *host, u32 proc,
-		 __be32 (*func)(struct svc_rqst *,  struct nlm_res *))
+		 __be32 (*func)(struct svc_rqst *,  struct lockd_res *))
 {
 	struct nlm_rqst	*call;
 	__be32 stat;
@@ -585,7 +585,7 @@ nlm4svc_callback(struct svc_rqst *rqstp, struct nlm_host *host, u32 proc,
 }
 
 static __be32
-__nlm4svc_proc_test_msg(struct svc_rqst *rqstp, struct nlm_res *resp)
+__nlm4svc_proc_test_msg(struct svc_rqst *rqstp, struct lockd_res *resp)
 {
 	struct nlm4_testargs_wrapper *argp = rqstp->rq_argp;
 	unsigned char type = argp->xdrgen.exclusive ? F_WRLCK : F_RDLCK;
@@ -645,7 +645,7 @@ static __be32 nlm4svc_proc_test_msg(struct svc_rqst *rqstp)
 }
 
 static __be32
-__nlm4svc_proc_lock_msg(struct svc_rqst *rqstp, struct nlm_res *resp)
+__nlm4svc_proc_lock_msg(struct svc_rqst *rqstp, struct lockd_res *resp)
 {
 	struct nlm4_lockargs_wrapper *argp = rqstp->rq_argp;
 	unsigned char type = argp->xdrgen.exclusive ? F_WRLCK : F_RDLCK;
@@ -707,7 +707,7 @@ static __be32 nlm4svc_proc_lock_msg(struct svc_rqst *rqstp)
 }
 
 static __be32
-__nlm4svc_proc_cancel_msg(struct svc_rqst *rqstp, struct nlm_res *resp)
+__nlm4svc_proc_cancel_msg(struct svc_rqst *rqstp, struct lockd_res *resp)
 {
 	struct nlm4_cancargs_wrapper *argp = rqstp->rq_argp;
 	unsigned char type = argp->xdrgen.exclusive ? F_WRLCK : F_RDLCK;
@@ -771,7 +771,7 @@ static __be32 nlm4svc_proc_cancel_msg(struct svc_rqst *rqstp)
 }
 
 static __be32
-__nlm4svc_proc_unlock_msg(struct svc_rqst *rqstp, struct nlm_res *resp)
+__nlm4svc_proc_unlock_msg(struct svc_rqst *rqstp, struct lockd_res *resp)
 {
 	struct nlm4_unlockargs_wrapper *argp = rqstp->rq_argp;
 	struct net *net = SVC_NET(rqstp);
@@ -834,7 +834,7 @@ static __be32 nlm4svc_proc_unlock_msg(struct svc_rqst *rqstp)
 }
 
 static __be32
-__nlm4svc_proc_granted_msg(struct svc_rqst *rqstp, struct nlm_res *resp)
+__nlm4svc_proc_granted_msg(struct svc_rqst *rqstp, struct lockd_res *resp)
 {
 	struct nlm4_testargs_wrapper *argp = rqstp->rq_argp;
 

kernelorg-mirror/linux_kernel_git_sashal_linux

[ Upstream commit 86ed2898fafb8106072f9db3887deec80222e6cf ]
Reviewed-by: Jeff Layton <jlayton@kernel.org>
[ Backport to 7.0: this tree predates the upstream commits that split
  fs/lockd/xdr4.c into clnt4xdr.c/svc4proc.c and that moved the NLMv1/v3
  svc_version tables and the nlmsvc_xdrstore union from svc.c into
  svcproc.c. Because of that, several sites where upstream had already
  converted away from "struct nlm_res" (leaving no textual diff for
  this rename) still spell it "struct nlm_res" here, and fs/lockd/xdr4.c
  (which does not exist upstream at this commit) also still uses it.
  Extended the mechanical rename to cover all of these additional
  "struct nlm_res" occurrences in fs/lockd/svc4proc.c and
  fs/lockd/xdr4.c so the type is renamed consistently tree-wide;
  dropped the hunk that added the svc_version tables/xdrstore union
  to svcproc.c, since that code does not exist in this file here (it
  still lives in fs/lockd/svc.c, unmodified). No functional change,
  and no other identifiers (nlm_args, nlm_cookie, nlm_lock) were
  touched, since their own renames have not been backported. ]
[ sashal: Reduced backport -- upstream 86ed2898fafb8 touches 8 file(s), this
  backport carries 9. Not backported here:
  fs/lockd/lockd.h
  fs/lockd/xdr.h
  This note is generated from the file lists only; see the resolution record
  for the reasoning. ]
Stable-dep-of: 641e5e208523 ("lockd, nfsd: RCU-protect nlmsvc_ops dispatch")
Signed-off-by: Sasha Levin <sashal@kernel.org>

fs/lockd/svc4proc.c
@@ -91,7 +91,7 @@ nlm4svc_proc_null(struct svc_rqst *rqstp)
  * TEST: Check for conflicting lock
  */
 static __be32
-__nlm4svc_proc_test(struct svc_rqst *rqstp, struct nlm_res *resp)
+__nlm4svc_proc_test(struct svc_rqst *rqstp, struct lockd_res *resp)
 {
 	struct nlm_args *argp = rqstp->rq_argp;
 	struct nlm_host	*host;
@@ -125,7 +125,7 @@ nlm4svc_proc_test(struct svc_rqst *rqstp)
 }
 
 static __be32
-__nlm4svc_proc_lock(struct svc_rqst *rqstp, struct nlm_res *resp)
+__nlm4svc_proc_lock(struct svc_rqst *rqstp, struct lockd_res *resp)
 {
 	struct nlm_args *argp = rqstp->rq_argp;
 	struct nlm_host	*host;
@@ -174,7 +174,7 @@ nlm4svc_proc_lock(struct svc_rqst *rqstp)
 }
 
 static __be32
-__nlm4svc_proc_cancel(struct svc_rqst *rqstp, struct nlm_res *resp)
+__nlm4svc_proc_cancel(struct svc_rqst *rqstp, struct lockd_res *resp)
 {
 	struct nlm_args *argp = rqstp->rq_argp;
 	struct nlm_host	*host;
@@ -214,7 +214,7 @@ nlm4svc_proc_cancel(struct svc_rqst *rqstp)
  * UNLOCK: release a lock
  */
 static __be32
-__nlm4svc_proc_unlock(struct svc_rqst *rqstp, struct nlm_res *resp)
+__nlm4svc_proc_unlock(struct svc_rqst *rqstp, struct lockd_res *resp)
 {
 	struct nlm_args *argp = rqstp->rq_argp;
 	struct nlm_host	*host;
@@ -255,7 +255,7 @@ nlm4svc_proc_unlock(struct svc_rqst *rqstp)
  * was granted
  */
 static __be32
-__nlm4svc_proc_granted(struct svc_rqst *rqstp, struct nlm_res *resp)
+__nlm4svc_proc_granted(struct svc_rqst *rqstp, struct lockd_res *resp)
 {
 	struct nlm_args *argp = rqstp->rq_argp;
 
@@ -296,7 +296,7 @@ static const struct rpc_call_ops nlm4svc_callback_ops = {
  * doesn't break any clients.
  */
 static __be32 nlm4svc_callback(struct svc_rqst *rqstp, u32 proc,
-		__be32 (*func)(struct svc_rqst *,  struct nlm_res *))
+		__be32 (*func)(struct svc_rqst *,  struct lockd_res *))
 {
 	struct nlm_args *argp = rqstp->rq_argp;
 	struct nlm_host	*host;
@@ -363,7 +363,7 @@ static __be32
 nlm4svc_proc_share(struct svc_rqst *rqstp)
 {
 	struct nlm_args *argp = rqstp->rq_argp;
-	struct nlm_res *resp = rqstp->rq_resp;
+	struct lockd_res *resp = rqstp->rq_resp;
 	struct nlm_host	*host;
 	struct nlm_file	*file;
 
@@ -398,7 +398,7 @@ static __be32
 nlm4svc_proc_unshare(struct svc_rqst *rqstp)
 {
 	struct nlm_args *argp = rqstp->rq_argp;
-	struct nlm_res *resp = rqstp->rq_resp;
+	struct lockd_res *resp = rqstp->rq_resp;
 	struct nlm_host	*host;
 	struct nlm_file	*file;
 
@@ -485,7 +485,7 @@ nlm4svc_proc_sm_notify(struct svc_rqst *rqstp)
 static __be32
 nlm4svc_proc_granted_res(struct svc_rqst *rqstp)
 {
-	struct nlm_res *argp = rqstp->rq_argp;
+	struct lockd_res *argp = rqstp->rq_argp;
 
         if (!nlmsvc_ops)
                 return rpc_success;
@@ -531,7 +531,7 @@ const struct svc_procedure nlmsvc_procedures4[24] = {
 		.pc_encode = nlm4svc_encode_testres,
 		.pc_argsize = sizeof(struct nlm_args),
 		.pc_argzero = sizeof(struct nlm_args),
-		.pc_ressize = sizeof(struct nlm_res),
+		.pc_ressize = sizeof(struct lockd_res),
 		.pc_xdrressize = Ck+St+2+No+Rg,
 		.pc_name = "TEST",
 	},
@@ -541,7 +541,7 @@ const struct svc_procedure nlmsvc_procedures4[24] = {
 		.pc_encode = nlm4svc_encode_res,
 		.pc_argsize = sizeof(struct nlm_args),
 		.pc_argzero = sizeof(struct nlm_args),
-		.pc_ressize = sizeof(struct nlm_res),
+		.pc_ressize = sizeof(struct lockd_res),
 		.pc_xdrressize = Ck+St,
 		.pc_name = "LOCK",
 	},
@@ -551,7 +551,7 @@ const struct svc_procedure nlmsvc_procedures4[24] = {
 		.pc_encode = nlm4svc_encode_res,
 		.pc_argsize = sizeof(struct nlm_args),
 		.pc_argzero = sizeof(struct nlm_args),
-		.pc_ressize = sizeof(struct nlm_res),
+		.pc_ressize = sizeof(struct lockd_res),
 		.pc_xdrressize = Ck+St,
 		.pc_name = "CANCEL",
 	},
@@ -561,7 +561,7 @@ const struct svc_procedure nlmsvc_procedures4[24] = {
 		.pc_encode = nlm4svc_encode_res,
 		.pc_argsize = sizeof(struct nlm_args),
 		.pc_argzero = sizeof(struct nlm_args),
-		.pc_ressize = sizeof(struct nlm_res),
+		.pc_ressize = sizeof(struct lockd_res),
 		.pc_xdrressize = Ck+St,
 		.pc_name = "UNLOCK",
 	},
@@ -571,7 +571,7 @@ const struct svc_procedure nlmsvc_procedures4[24] = {
 		.pc_encode = nlm4svc_encode_res,
 		.pc_argsize = sizeof(struct nlm_args),
 		.pc_argzero = sizeof(struct nlm_args),
-		.pc_ressize = sizeof(struct nlm_res),
+		.pc_ressize = sizeof(struct lockd_res),
 		.pc_xdrressize = Ck+St,
 		.pc_name = "GRANTED",
 	},
@@ -629,8 +629,8 @@ const struct svc_procedure nlmsvc_procedures4[24] = {
 		.pc_func = nlm4svc_proc_null,
 		.pc_decode = nlm4svc_decode_void,
 		.pc_encode = nlm4svc_encode_void,
-		.pc_argsize = sizeof(struct nlm_res),
-		.pc_argzero = sizeof(struct nlm_res),
+		.pc_argsize = sizeof(struct lockd_res),
+		.pc_argzero = sizeof(struct lockd_res),
 		.pc_ressize = sizeof(struct nlm_void),
 		.pc_xdrressize = St,
 		.pc_name = "TEST_RES",
@@ -639,8 +639,8 @@ const struct svc_procedure nlmsvc_procedures4[24] = {
 		.pc_func = nlm4svc_proc_null,
 		.pc_decode = nlm4svc_decode_void,
 		.pc_encode = nlm4svc_encode_void,
-		.pc_argsize = sizeof(struct nlm_res),
-		.pc_argzero = sizeof(struct nlm_res),
+		.pc_argsize = sizeof(struct lockd_res),
+		.pc_argzero = sizeof(struct lockd_res),
 		.pc_ressize = sizeof(struct nlm_void),
 		.pc_xdrressize = St,
 		.pc_name = "LOCK_RES",
@@ -649,8 +649,8 @@ const struct svc_procedure nlmsvc_procedures4[24] = {
 		.pc_func = nlm4svc_proc_null,
 		.pc_decode = nlm4svc_decode_void,
 		.pc_encode = nlm4svc_encode_void,
-		.pc_argsize = sizeof(struct nlm_res),
-		.pc_argzero = sizeof(struct nlm_res),
+		.pc_argsize = sizeof(struct lockd_res),
+		.pc_argzero = sizeof(struct lockd_res),
 		.pc_ressize = sizeof(struct nlm_void),
 		.pc_xdrressize = St,
 		.pc_name = "CANCEL_RES",
@@ -659,8 +659,8 @@ const struct svc_procedure nlmsvc_procedures4[24] = {
 		.pc_func = nlm4svc_proc_null,
 		.pc_decode = nlm4svc_decode_void,
 		.pc_encode = nlm4svc_encode_void,
-		.pc_argsize = sizeof(struct nlm_res),
-		.pc_argzero = sizeof(struct nlm_res),
+		.pc_argsize = sizeof(struct lockd_res),
+		.pc_argzero = sizeof(struct lockd_res),
 		.pc_ressize = sizeof(struct nlm_void),
 		.pc_xdrressize = St,
 		.pc_name = "UNLOCK_RES",
@@ -669,8 +669,8 @@ const struct svc_procedure nlmsvc_procedures4[24] = {
 		.pc_func = nlm4svc_proc_granted_res,
 		.pc_decode = nlm4svc_decode_res,
 		.pc_encode = nlm4svc_encode_void,
-		.pc_argsize = sizeof(struct nlm_res),
-		.pc_argzero = sizeof(struct nlm_res),
+		.pc_argsize = sizeof(struct lockd_res),
+		.pc_argzero = sizeof(struct lockd_res),
 		.pc_ressize = sizeof(struct nlm_void),
 		.pc_xdrressize = St,
 		.pc_name = "GRANTED_RES",
@@ -721,7 +721,7 @@ const struct svc_procedure nlmsvc_procedures4[24] = {
 		.pc_encode = nlm4svc_encode_shareres,
 		.pc_argsize = sizeof(struct nlm_args),
 		.pc_argzero = sizeof(struct nlm_args),
-		.pc_ressize = sizeof(struct nlm_res),
+		.pc_ressize = sizeof(struct lockd_res),
 		.pc_xdrressize = Ck+St+1,
 		.pc_name = "SHARE",
 	},
@@ -731,7 +731,7 @@ const struct svc_procedure nlmsvc_procedures4[24] = {
 		.pc_encode = nlm4svc_encode_shareres,
 		.pc_argsize = sizeof(struct nlm_args),
 		.pc_argzero = sizeof(struct nlm_args),
-		.pc_ressize = sizeof(struct nlm_res),
+		.pc_ressize = sizeof(struct lockd_res),
 		.pc_xdrressize = Ck+St+1,
 		.pc_name = "UNSHARE",
 	},
@@ -741,7 +741,7 @@ const struct svc_procedure nlmsvc_procedures4[24] = {
 		.pc_encode = nlm4svc_encode_res,
 		.pc_argsize = sizeof(struct nlm_args),
 		.pc_argzero = sizeof(struct nlm_args),
-		.pc_ressize = sizeof(struct nlm_res),
+		.pc_ressize = sizeof(struct lockd_res),
 		.pc_xdrressize = Ck+St,
 		.pc_name = "NM_LOCK",
 	},